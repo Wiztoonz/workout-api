95b0c019a64c18d44d27f296dca577fa03d9b3a7
Oleg Rosynskyi <olegrosynskyi@gmail.com>
2022-07-16 23:07:13 +0300

    cors

--- src/main/java/com/sport/workout/controller/AuthController.java
+++ src/main/java/com/sport/workout/controller/AuthController.java
@@ -12,12 +12,10 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/v1/auth/")
 public class AuthController {
 
